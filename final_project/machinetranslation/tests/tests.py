4ee9a298163f6bf88d38dce93e6e9d28cc51db77
adityakahb <aditya.bhakay@live.com>
2022-10-19 15:36:46 +0530

    Updated tests file

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -2,20 +2,20 @@
     Tests file to test the translator functionality
 """
 import unittest
-from translator import french_to_english
 from translator import english_to_french
+from translator import french_to_english
 
 
 class TestLanguageTranslator(unittest.TestCase):
     def test_english_to_french(self):
         self.assertEqual(english_to_french('Hello'), 'Bonjour')
-        self.assertEqual(english_to_french(
-            None), 'Could not process English Text')
+        self.assertNotEqual(english_to_french(
+            None), 'Bonjour')
 
     def test_french_to_english(self):
         self.assertEqual(french_to_english('Bonjour'), 'Hello')
-        self.assertEqual(french_to_english(
-            None), 'Could not process French Text')
+        self.assertNotEqual(french_to_english(
+            None), 'Hello')
 
 
 if __name__ == '__main__':
